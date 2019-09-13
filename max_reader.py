79dc5300d11f0e9201d07413600bf2d213acc340
max <mwt365@gmail.com>
2019-09-13 10:50:08 -0700

    Init

--- max_reader.py
+++ max_reader.py
@@ -1,17 +1,21 @@
 import struct
 
-with open("out.txt", "rb") as test_file:
+with open("GEN3CH_4_009.dig", "rb") as test_file:
 	with open("output.txt", 'w+') as output:
 		data = test_file.read()
 		words = str(data[:513],'ascii')
 		output = open("guru99.txt","w+")
 		output.write(words)
 		print (len(data))
-		while len(data)>4:
-			ints = struct.unpack('=H', data[600:602])
-			print(ints)
-			# strInt = str(ints[1])
-			# output.write(strInt)
+		print(struct.calcsize('H'))
+		# for i in range(int(len(data)/2)):
+		# 	if (len(data) >2):
+		intArray = []
+		print(len(data))
+		intArray += struct.unpack("B" * (len(data) -512), data[512:-5])
+		print(intArray)
+		# strInt = str(ints[1])
+		# output.write(strInt)
 
 
 
