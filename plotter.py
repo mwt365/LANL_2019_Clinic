a88e9efb6421af8c8c75affa44788531e9941802
trevor-walker32 <trevorwalker32.tw@gmail.com>
2019-09-20 11:45:03 -0700

    cleaning up branch

--- plotter.py
+++ plotter.py
@@ -1,6 +1,8 @@
 import cm_xml_to_matplotlib as cm
 
-mycmap = cm.make_cmap('7-section-muted.xml') #make the Matplotlib compatible colormap
+mycmap = cm.make_cmap('./xml_cm_files/.xml') #make the Matplotlib compatible colormap
 ## to use colormap: matplotlib.pyplot.imshow(your_image, cmap=matplotlib.pyplot.get_cmap(mycmap))
 
 cm.plot_cmap(mycmap) #plot an 8 by 1 copy of the colormap
+
+cm.cmap_matrix(mycmap)
