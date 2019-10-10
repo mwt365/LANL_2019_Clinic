bd6bd4eb7044545212574f9b78be4218e3ebdc30
NickHMC <35875882+NickHMC@users.noreply.github.com>
2019-10-10 03:13:02 -0700

    Update extractSignal.py
    updated the extraction to keep multiple things for the output at each time slot.

--- extractSignal.py
+++ extractSignal.py
@@ -1,10 +1,11 @@
 import numpy as np
-from spectrogram import Spectrogram
 
-def extractSignal(sgram, lowerVelocityThreshold, upperVelocityThreshold, plotAbove):
+def extractSignal(intensityMatrix, velocities, lowerVelocityThreshold, upperVelocityThreshold, plotAbove):
     """
     Input:
         sgram: a spectrogram object
+        intensityMatrix: a 2d array [velocity][time] and each cell corresponds to the intensity at that point.
+        velocities: a 1d array of the velocities being consider.
         lowerVelocityThreshold: what you consider to be the lower threshold on an 
         interesting velocity. 
             Assumed to be a valid index for sgram's intensity array.
@@ -17,11 +18,22 @@ def extractSignal(sgram, lowerVelocityThreshold, upperVelocityThreshold, plotAbo
         indicating the highest intensity value at the given time slice.
     """
     
-    signalVelocityIndecies = np.zeros(len(sgram.t), dtype=int)
+    time_then_velocity_intensities = np.transpose(intensityMatrix)
 
-    time_then_velocity_intensities = np.transpose(sgram.intensity)
+    signalVelocityIndecies = np.zeros(time_then_velocity_intensities.shape[0], dtype = np.int64)
 
+    if time_then_velocity_intensities.shape[-1] != len(velocities):
+        raise ValueError("The intensities matrix and the velocity matrix mismatch in shape.")
+
+    dataPoints = []
     for i in range(len(signalVelocityIndecies)):
         signalVelocityIndecies[i] = np.argmax(time_then_velocity_intensities[i][lowerVelocityThreshold:upperVelocityThreshold+1]) + lowerVelocityThreshold
+        AboveThresData = []
+        for j in range(lowerVelocityThreshold, upperVelocityThreshold+1):
+            if time_then_velocity_intensities[i][j] >= plotAbove * time_then_velocity_intensities[i][signalVelocityIndecies[i]]:
+                AboveThresData.append(j)
+        dataPoints.append(np.array(AboveThresData))
     
-    return signalVelocityIndecies
+    print(dataPoints)
+
+    return dataPoints
