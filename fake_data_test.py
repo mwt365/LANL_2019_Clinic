a055f2f4b2f5a3178a32ecb68831cb8ebe30c389
mwt365 <mwt365@gmail.com>
2019-10-11 12:01:46 -0700

    Sample testi file of real function

---
+++ fake_data_test.py
@@ -0,0 +1,16 @@
+#fake_data_test.py
+#tests fake_data.py
+
+import pytest
+from fake_data import *
+import os.path
+from os import path
+
+fakeObject = make_dig_file('fake_data_test.dig',
+        [0, 1e-5, 2e-5, 4e-5],
+        [1000, 1000, 5000, 3000]
+    )
+
+assert path.exists('fake_data_test.dig'), 'file not created'
+
+os.remove('fake_data_test.dig')
