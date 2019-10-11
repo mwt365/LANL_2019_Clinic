d3996a8752027e12b183343f6999c9891261124b
mwt365 <mwt365@gmail.com>
2019-10-11 12:01:14 -0700

    sample pytest file

---
+++ sample_test.py
@@ -0,0 +1,16 @@
+#test.py
+#lanl2019
+#testing framework using pytest
+
+import pytest
+
+
+def test_file1_method1():
+	x=5
+	y=6
+	assert x+1 == y,"test failed"
+	assert x == y,"test failed"
+def test_file1_method2():
+	x=5
+	y=6
+	assert x+1 == y,"test failed" 
