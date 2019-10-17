15612ccd0074e18708a1e2185fdd7d44ed866ccc
NickHMC <nkoskelo@hmc.edu>
2019-10-17 03:05:45 -0700

    Added the seam carving for the signal extraction. Currently has a memory bug. (It uses too much). We can really just store the lastest two columns for the DP table but I will need to update the parent table as well.

--- sc.py
+++ sc.py
@@ -80,7 +80,8 @@ def find_seams(two_dimensional_array: list, num_signals: int, t_end:int):
     # assert isinstance(two_dimensional_array[0], list)
     assert isinstance(two_dimensional_array[0][0], float)
 
-    rez = [[two_dimensional_array[j][i] for j in range(len(two_dimensional_array))] for i in range(len(two_dimensional_array[0]))] 
+    rez = [[two_dimensional_array[j][i] for j in range(len(two_dimensional_array))] for i in range(len(two_dimensional_array[0]))] # This looks like a transpose call
+    # try rez = np.transpose(two_dimensional_array)
 
     potential_seams = find_potential_seams(rez, num_signals)
 
@@ -208,35 +209,88 @@ def within_range(test, percent, base):
     return lowRange <= test and test <= highRange
 
 
+def seamSignalExtraction(intensityMatrix, startTime:int, stopTime:int, width:int, bottomIndex:int, metricFunction = None):
+    """
+        Input:
+            intensityMatrix: a 2d array [velocity][time] and each cell 
+                corresponds to the intensity at that point.
+            startTime: index corresponding to the start time in the intensity 
+                matrix of the signal being considered.
+            stopTime: index corresponding to the stop time in the intensity
+                matrix of the signal being considered.
+            width:
+                How wide of a window that is being considered at
+                    each time step. The window will be split evenly up and down
+            bottomIndex: Upper bound on the velocity. 
+                Assumed to be a valid index for sgram's intensity array.
+            metricFunction: function that will be minimized to determine the most 
+                well connected signal. 
+                Defaults to None:
+                    minimizing the Manhattan distance between neighboring pixels.
+                Needs to take two numbers and return a number.
+    Output:
+        list of indices that should be plotted as the signal as an array of indices
+        indicating the intensity values that correspond to the most 
+        connected signal as defined by the metric.
+    """
 
+    if stopTime <= startTime:
+        raise ValueError("Stop Time is assumed to be greater than start time.")
+
+    if metricFunction != None:
+        # Check if metricFunction is a function that takes in two numbers and returns a number
+        if not callable(metricFunction):
+            raise ValueError("metricFunction must either be None or a function \
+                that takes two numbers and returns a number.")
+        raise TypeError("Method has not been implemented to take arbitrary functions.")
+    else:
+        numTimeSteps = stopTime - startTime
+        velocities = np.zeros(numTimeSteps)
+        # This will hold the answers that we are looking for to return.
+
+        startIndex = np.argmax(intensityMatrix[:][startTime])
+
+        top = startIndex + round((width/2)(numTimeSteps)+0.01) 
+            # to get it to round up to 1 if it is 0.5
+        bottomDP = max(startIndex - round((width/2)(numTimeSteps)+0.01), bottomIndex + 1) 
+            # to get it to round up to 1 if it is 0.5
+
+
+        DPTable = np.zeros(((top - bottomDP)*(numTimeSteps), numTimeSteps + 1))
+        parentTable = np.zeros(DPTable.shape())
+
+        for timeIndex in range(stopTime-1, startTime-1, -1):
+            for velocityIndex in range(0, top+1-bottomDP):
+                bestSoFar = np.Infinity
+                bestPointer = None
+                for testIndex in range(max(velocityIndex - round(width/2 + 0.01), bottomDP), min(top, velocityIndex + round(width/2+0.01)+1)):
+                    testIndex -= bottomDP
+                    current = np.abs(intensityMatrix[testIndex][timeIndex+1] - intensityMatrix[velocityIndex][timeIndex]) + DPTable[testIndex][timeIndex+1]
+                    if current < bestSoFar:
+                        bestSoFar = current
+                        bestPointer = testIndex
+                DPTable[velocityIndex][timeIndex] = bestSoFar
+                parentTable[velocityIndex][timeIndex] = bestPointer
+
+
+        # Now for the reconstruction.
+        currentPointer = startIndex - bottomDP
+        for timeIndex in range(startTime, stopTime+1):
+            velocities[timeIndex] = currentPointer
+            currentPointer = parentTable[currentPointer][timeIndex]
+
+
+        print("Here is the full DP Table for debugging purposes")
+
+        print(DPTable)
+
+        return velocities
 
 
 if __name__ == "__main__":
-
-    sp_nl = spctgrm.Spectrogram('GEN3CH_4_009.dig')
-    sgram_nl = sp_nl.spectrogram_no_log(0, 50e-6)
-
-    # intensities = [np.sum(sgram_nl['spectrogram'], axis=1)]
-    intensities = []
-
-    for inner_array in sgram_nl['spectrogram']:
-        max_intensity = np.max(inner_array)
-        intensities.append(max_intensity)
-
-    max_intensity = np.max(intensities)
-
-    # baseline_velocity_index = [np.argmax(intensities)]
-    # baseline_velocity = sgram_nl['v'][baseline_velocity_index]
-
-    print(max_intensity)
-    print("\n")
-    print("\n")
-
-    # print(sgram_nl['spectrogram'].shape)
-
-    # print(sgram_nl['v'])
-    # print(sgram_nl['t'])
-    # print(sgram_nl['spectrogram'])
+    fname = "GEN3CH_4_009.dig"
+    sp_nl = spctgrm.Spectrogram(fname)
+    sgram_nl = sp_nl.spectrogram(0, 50e-6)
 
     sp = sgram_nl['spectrogram']
 
@@ -259,7 +313,6 @@ if __name__ == "__main__":
     print("baseline velocity: ", baseline_velocity)
     print("baseline intensity: ", baseline_intensity)
 
-    # print(sgram_nl['v'])
-    
 
+    # print(sgram_nl['v'])
         
