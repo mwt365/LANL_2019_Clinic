3de73a7ebf7e817841d86d1090e93c48ece9683d
Peter Saeta <saeta@hmc.edu>
2019-11-27 15:12:30 -0800

    Added a shape option so the moving average can be weighted by a rectangle, triangle, or gaussian. Running the test code at the bottom of the file suggests that we want to use a triangle, so I have made that shape the default.

--- moving_average.py
+++ moving_average.py
@@ -10,12 +10,40 @@
 import numpy as np
 
 
-def moving_average(x: np.ndarray, n=3):
+def moving_average(x: np.ndarray, n=3, shape='triangle'):
     """
-    prepare a smoothed version by averaging values
-    from n to the left to n to the right
+    Prepare a smoothed version by averaging values
+    from n to the left to n to the right. Shape may be one
+    of {rectangle, triangle, gaussian}
     """
+    pts = 2 * n + 1
+    offsets = np.linspace(-n, n, pts, dtype=np.int32)
+    if shape == 'triangle':
+        weights = -np.abs(offsets) + n + 1
+    elif shape == 'gaussian':
+        weights = np.exp(-0.5 * np.linspace(-1.25, 1.25, pts)**2)
+    else:
+        weights = np.ones(pts)
+    total = np.sum(weights)
+    weights = np.asarray(weights, dtype=np.float) / total
+
     sm = np.zeros(len(x))
-    for i in range(-n, n + 1):
-        sm += np.roll(x, i)
-    return sm / (1 + 2 * n)
+    for i, w in zip(offsets, weights):
+        sm += w * np.roll(x, i)
+    return sm
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from numpy.random import Generator, PCG64
+    rg = Generator(PCG64())
+    x = np.linspace(-4, 4, 161)
+    noisy = rg.standard_normal(len(x))
+    noisy += 8 * np.exp(-1.0 * (x + 1)**2)
+    noisy += 20 * np.exp(-8.0 * (x - 1)**2)
+    plt.plot(x, noisy, 'ko', alpha=0.5)
+    shapes = ('rectangle', 'triangle', 'gaussian')
+    for shape in shapes:
+        plt.plot(x, moving_average(noisy, n=10, shape=shape))
+    plt.legend(['data'] + list(shapes))
+    plt.show()
