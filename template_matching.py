4584009724b06cee19a8b0e93bb109760144fe2c
trevor-walker32 <trevorwalker32.tw@gmail.com>
2020-01-27 23:32:41 -0800

    cleaned up file, added helper functions

--- template_matching.py
+++ template_matching.py
@@ -2,7 +2,7 @@
 import numpy as np
 from spectrogram import Spectrogram
 import peak_follower
-import baselines
+import baselines as bls
 
 
 class Template:
@@ -17,7 +17,6 @@ class Template:
         self.height = height if height != None else 0
         self.values = values if values != None else []
         
-
 
 
 start_pattern = [
@@ -39,7 +38,7 @@ start_pattern3 = [
 
 def calculate_score(index, template, intensities, time_index):
 
-    template_sum = 0 
+    template_sum = 0
 
     for values in template.values:
 
@@ -50,55 +49,118 @@ def calculate_score(index, template, intensities, time_index):
     return template_sum
 
 
+def find_potential_baselines(sgram):
 
-# def setup(path):
+    baselines = []
+    hoods = bls.baselines_by_fft(sgram)
 
-#     os.chdir(path)
-#     df = DigFile('CH_2_009.dig')
-#     baselines_v = []
-#     sgram = Spectrogram(df, 0.0, 60.0e-6, form='db')
-#     hoods = baselines.baselines_by_fft(sgram)
+    for n, h in enumerate(hoods):
+        max_v = 0
+        max_i = 0
+        # print(f"Peak {n}\nVelocity{n}\tIntensity{n}")
+        v, i = h
+        for j in range(len(v)):
+            # print(f"{v[j]:.4f}\t{i[j]:.4f}")
+            if i[j] > max_i:
+                max_i = i[j]
+                max_v = v[j]    
+        baselines.append(max_v)
 
-#     # lower_bound_t = input("enter a time to the left of the jumpoff point.")
-#     # lower_bound_t = int(lower_bound_t) * 10**-6
-#     # upper_bound_t = input("enter a time to the right of the jumpoff point.")
-#     # upper_bound_t = int(upper_bound_t) * 10**-6
+    new_baselines = []
 
-#     # upper_bound_v = input("enter a velocity above the jumpoff point.")
-#     # upper_bound_v = int(upper_bound_v)
-#     # lower_bound_v = input("enter a velocity below the jumpoff point.")
-#     # lower_bound_v = int(lower_bound_v)
+    new_baselines.append(baselines[1])
 
-#     lower_bound_t = 10 * 10**-6
-#     upper_bound_t = 14 * 10**-6
-#     upper_bound_v = sgram._velocity_to_index(3500)
-#     lower_bound_v = sgram._velocity_to_index(2000)
+    # for baseline in baselines:
+    #     print("is there a baseline at: ", baseline, "?", end=" ")
+    #     ans = input("(y/n)\n")
+    #     if ans == "y":
+    #         new_baselines.append(baseline)
+    #     else:
+    #         continue
 
-#     tvals, vvals, ivals = sgram.slice((lower_bound_t, upper_bound_t), (lower_bound_v, upper_bound_v))
-
-#     for n, h in enumerate(hoods):
-#         max_v = 0
-#         max_i = 0
-#         # print(f"Peak {n}\nVelocity{n}\tIntensity{n}")
-#         v, i = h
-#         for j in range(len(v)):
-#             # print(f"{v[j]:.4f}\t{i[j]:.4f}")
-#             if i[j] > max_i:
-#                 max_i = i[j]
-#                 max_v = v[j]    
-#         # print("\n")
-#         baselines_v.append(max_v)
+    return new_baselines
 
 
-#     actual_baselines = []
+def find_start_time(sgram, baselines):
 
-#     for baseline in baselines_v:
-#         print("is there a baseline at: ", baseline, "?", end=" ")
-#         ans = input("(y/n)\n")
-#         if ans == "y":
-#             actual_baselines.append(baseline)
-#         else:
-#             continue
+    time_index = None
+
+    for baseline in baselines:
+        ans = input("where does the start begin? (microseconds)")
+        try:
+            start_time = int(ans) * 10**-6
+            # baseline_index = sgram._velocity_to_index(baseline)
+            time_index = sgram._time_to_index(start_time)
+        except:
+            print("input can not be converted to integer")
+            return None
+
+    return time_index
+    
+
+def find_regions(sgram, velo_bounds=None, time_bounds=None):
+
+    if velo_bounds is not None:
+        assert isinstance(velo_bounds, tuple)
+        lower_velo = velo_bounds[0]
+        upper_velo = velo_bounds[1]
+    elif velo_bounds is None:
+        upper_velo_index = sgram.intensity.shape[0]
+        upper_velo = sgram.velocity[upper_velo_index]
+        lower_velo = sgram.velocity[0]
+    if time_bounds is not None:
+        assert isinstance(time_bounds, tuple)
+        lower_time = time_bounds[0]
+        upper_time = time_bounds[1]
+    elif time_bounds is None:
+        upper_time_index = sgram.intensity.shape[1]
+        upper_time = sgram.time[upper_time_index]
+        lower_time = sgram.time[0]
+
+    baselines = find_potential_baselines(sgram)
+
+    time_index = find_start_time(sgram, baselines)
+
+    width = 4
+    height = 3
+
+    upper_velo_index = sgram._velocity_to_index(upper_velo)
+    lower_velo_index = sgram._velocity_to_index(lower_velo)
+
+    newTemplate = Template(width, height, start_pattern3)
+
+    time_max = sgram.intensity.shape[1]
+    velocity_max = sgram.intensity.shape[0]
+
+    scores = {}
+
+    # print(time_max)
+    # print(velocity_max)
+    # print("upper: ",upper_velo_index)
+    # print("lower: ",lower_velo_index)
+    # print(sgram.intensity)
+    # print(newTemplate.values)
+    # print(time_index)
+
+    for velocity_index in range(upper_velo_index, lower_velo_index, -1):
+
+        score = calculate_score(velocity_index, newTemplate, sgram.intensity, time_index)
+        scores[velocity_index] = score
+
+    # print(scores)
+
+    sorted_scores = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1])}
+    indicies = list(sorted_scores.keys())
+
+    return indicies
+
+
+def find_potenital_start_points(sgram, indicies):
+
+    for i in range(len(indicies)-1, len(indicies)-10, -1):
+        velocity = sgram.velocity[indicies[i]]
+        print("velocity: ",velocity, "\n")
+        # print("score: ", sorted_scores[indicies[i]],"\n")
 
 
 
@@ -113,87 +175,30 @@ if __name__ == '__main__':
     baselines_v = []
 
     sgram = Spectrogram(df, 0.0, 60.0e-6, form='db')
-    hoods = baselines.baselines_by_fft(sgram)
 
-    # lower_bound_t = input("enter a time to the left of the jumpoff point.")
+    # lower_bound_t = input("enter a time to the left of the jumpoff point: \n")
     # lower_bound_t = int(lower_bound_t) * 10**-6
-    # upper_bound_t = input("enter a time to the right of the jumpoff point.")
+    # upper_bound_t = input("enter a time to the right of the jumpoff point: \n")
     # upper_bound_t = int(upper_bound_t) * 10**-6
 
-    # upper_bound_v = input("enter a velocity above the jumpoff point.")
+    # upper_bound_v = input("enter a velocity above the jumpoff point: \n")
     # upper_bound_v = int(upper_bound_v)
-    # lower_bound_v = input("enter a velocity below the jumpoff point.")
+    # lower_bound_v = input("enter a velocity below the jumpoff point: \n")
     # lower_bound_v = int(lower_bound_v)
+
 
     lower_bound_t = 10 * 10**-6
     upper_bound_t = 14 * 10**-6
-    upper_bound_v = sgram._velocity_to_index(3500)
-    lower_bound_v = sgram._velocity_to_index(2000)
-
-    tvals, vvals, ivals = sgram.slice((lower_bound_t, upper_bound_t), (lower_bound_v, upper_bound_v))
+    upper_bound_v = 3700
+    lower_bound_v = 2000
 
 
-    for n, h in enumerate(hoods):
-        max_v = 0
-        max_i = 0
-        # print(f"Peak {n}\nVelocity{n}\tIntensity{n}")
-        v, i = h
-        for j in range(len(v)):
-            # print(f"{v[j]:.4f}\t{i[j]:.4f}")
-            if i[j] > max_i:
-                max_i = i[j]
-                max_v = v[j]    
-        # print("\n")
-        baselines_v.append(max_v)
+    time_bounds = (lower_bound_t, upper_bound_t)
+    velo_bounds = (lower_bound_v, upper_bound_v)
 
 
-    actual_baselines = []
+    indicies = find_regions(sgram, velo_bounds, time_bounds)
 
-    # for baseline in baselines_v:
-    #     print("is there a baseline at: ", baseline, "?", end=" ")
-    #     ans = input("(y/n)\n")
-    #     if ans == "y":
-    #         actual_baselines.append(baseline)
-    #     else:
-    #         continue
-    
-    actual_baselines.append(baselines_v[1])
-
-    for baseline in actual_baselines:
-        ans = input("where does the start begin? (microseconds)")
-        try: 
-            start_time = int(ans) * 10**-6
-            # baseline_index = sgram._velocity_to_index(baseline)
-            time_index = sgram._time_to_index(start_time)
-
-        except:
-            print("input can not be converted to integer")
-            break
-
-    width = 4
-    height = 3
-
-    newTemplate = Template(width, height, start_pattern3)
-
-    time_max = sgram.intensity.shape[1]
-    velocity_max = sgram.intensity.shape[0]
-
-    scores = {}
-
-    for velocity_index in range(upper_bound_v, lower_bound_v, -1):
-        
-        score = calculate_score(velocity_index, newTemplate, sgram.intensity, time_index)
-        # score = calculate_score(velocity_index, newTemplate, ivals, time_index)
-        scores[velocity_index] = score
-
-    
-    sorted_scores = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1])}
-    indicies = list(sorted_scores.keys())
-
-    for i in range(len(indicies)-1, len(indicies)-10, -1):
-        velocity = sgram.velocity[indicies[i]]
-        print("velocity: ",velocity)
-        print("score: ", sorted_scores[indicies[i]],"\n")
+    find_potenital_start_points(sgram, indicies)
 
 
-    # print(start_time)
