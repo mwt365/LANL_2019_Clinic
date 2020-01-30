5be97390c4681b0dfdb6ee4075c9406aadc0a634
NickHMC <nkoskelo@hmc.edu>
2020-01-30 11:58:42 -0800

    connected the 2

--- imageSegmentation.py
+++ imageSegmentation.py
@@ -4,6 +4,8 @@ from spectrogram import Spectrogram
 # Potential Graph algorithms that can be used to solve the network flow problem.
 from networkx.algorithms.flow import *
 
+from ImageProcessing.NetworkFlow.gaussianMixtureModels import GMM_Image_Seg
+
 if False:
     G = nx.DiGraph()
     G.add_edge('x',tuple(['v1', 'a']), capacity=3.0)
@@ -12,7 +14,7 @@ if False:
     G.add_edge(tuple(['v2', 'b']),tuple(['v3', 'c']), capacity=5.0)
     G.add_edge(tuple(['v2', 'b']),tuple(['v4', 'd']), capacity=4.0)
     G.add_edge(tuple(['v4', 'd']),tuple(['v5', 'e']), capacity=2.0)
-    G.add_edge(tuple(['v3', 'c']),'y', capacity=2.0)
+    G.add_edge(tuple(['v3', 'c']),'y', capacity=2.2)
     G.add_edge(tuple(['v5', 'e']),'y', capacity=3.0)
     R = edmonds_karp(G, 'x', 'y')
     flow_value = nx.maximum_flow_value(G, 'x', 'y')
@@ -22,7 +24,7 @@ if False:
     print("This is the residual graph's adjacency list",list(R.adjacency()))
 
 
-def setUpGraph(SpectrogramObject:Spectrogram, foregroundPoints:list, backgroundPoints:list):
+def setUpGraph(SpectrogramObject:Spectrogram):
     G = nx.DiGraph()
 
     t = SpectrogramObject.time
@@ -30,16 +32,16 @@ def setUpGraph(SpectrogramObject:Spectrogram, foregroundPoints:list, backgroundP
     Intensity = SpectrogramObject.intensity
     sigma = 30
 
-    hyperLambda = 0.5
+    GMM = GMM_Image_Seg(Intensity, verbose=True)
+
+    probabilities = GMM.computeProbabilities(Intensity)
+
+    foregroundLikelihood = probabilities[:,0]/np.sum(probabilities, axis=1)
 
     count = 0
     lent = len(t)
     lenv = len(velocity)
     maxCount = lent*lenv
-
-    foreGroundCost = foregroundLikelihood(SpectrogramObject, foregroundPoints, sigma)
-    backGroundCost = foregroundLikelihood(SpectrogramObject, backgroundPoints, sigma)
-
 
     for velInd in range(lenv):
         for timeInd in range(lent):
@@ -58,19 +60,16 @@ def setUpGraph(SpectrogramObject:Spectrogram, foregroundPoints:list, backgroundP
             if timeInd - 1 >= 0:
                 cap4 = penaltyFunction(inten1, Intensity[velInd][timeInd-1], sigma)
                 G.add_edge(u, u-1, capacity = cap4)
-            capacityFromS = 0
-            capacityToT = backGroundCost
-            if (timeInd, velInd) not in foregroundPoints and (timeInd, velInd) not in backgroundPoints:
-                capacityFromS = hyperLambda * Rvalue(inten1, backgroundPoints)
-                capacityToT = hyperLambda * Rvalue(inten1, foregroundPoints)
-            elif (timeInd, velInd) in foregroundPoints:
-                capacityFromS = foreGroundCost
-                capacityToT = 0
-            
+
+            probabilities = GMM.computeProbabilities(inten1)
+
+            capacityFromS = foregroundLikelihood[u]
+            capacityToT = 1 - capacityFromS
+
             G.add_edge("s", u, capacity = capacityFromS)            
             G.add_edge(u, "t", capacity = capacityToT)
             count += 1
-            if count%1e6 == 0:
+            if count%1e4 == 0:
                 print("I have completed", count, "out of", maxCount)
 
     # for (timeInd, velInd) in foregroundPoints:
@@ -86,52 +85,18 @@ def setUpGraph(SpectrogramObject:Spectrogram, foregroundPoints:list, backgroundP
 def penaltyFunction(Intensity1, Intensity2, sigma):
     return np.exp(-(np.square(Intensity1-Intensity2)/(2*np.square(sigma))))
 
-def foregroundLikelihood(SpectrogramObject:Spectrogram, foregroundPoints:list, sigma:int):
-    
-    tLen = len(SpectrogramObject.time)
-    vLen = len(SpectrogramObject.velocity)
-    Intensity = SpectrogramObject.intensity
-    maxPenalty = 0
-    for (timeInd, velInd) in foregroundPoints:
-        inten1 = Intensity[velInd][timeInd]
-        if velInd + 1 < vLen:
-            cap1 = penaltyFunction(inten1, Intensity[velInd+1][timeInd], sigma)
-            maxPenalty = max(maxPenalty, cap1)
-        if velInd - 1 >= 0:
-            cap2 = penaltyFunction(inten1, Intensity[velInd-1][timeInd], sigma)
-            maxPenalty = max(maxPenalty, cap2)
-        if timeInd + 1 < tLen:
-            cap3 = penaltyFunction(inten1, Intensity[velInd][timeInd+1], sigma)
-            maxPenalty = max(maxPenalty, cap3)
-        if timeInd - 1 >= 0:
-            cap4 = penaltyFunction(inten1, Intensity[velInd][timeInd-1], sigma)
-            maxPenalty = max(maxPenalty, cap4)
-    return 1 + maxPenalty
 
-# def backgroundLikelihood(SpectrogramObject:Spectrogram, pixel:int, tLen:int):
-#     return 1 - foregroundLikelihood(SpectrogramObject, pixel, tLen)
-
-
-def Rvalue(pixelIntensity:float, intensityList:list):
+def segmentImage(SpectrogramObject:Spectrogram):
     """
-    Based upon this paper:
-        https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=17&cad=rja&uact=8&ved=2ahUKEwjek92Qk6PnAhXFPn0KHZeaDf44ChAWMAZ6BAgHEAE&url=http%3A%2F%2Fyadda.icm.edu.pl%2Fyadda%2Felement%2Fbwmeta1.element.baztech-article-AGH1-0028-0094%2Fc%2FFabijanska.pdf&usg=AOvVaw0Xm9HxUAa7BUn6dqAPQ7qI
-
-    """
-    mean = np.mean(intensityList)
-
-    return 1 - (pixelIntensity - mean)/mean
-
-
-def segmentImage(SpectrogramObject:Spectrogram, foregroundPoints:list, backgroundPoints:list):
-    """
-        foregroundPoints: list of time, velocity coordinates that correspond to the signal.
-        backgroundPoints: list of time, velocity coordinates that correspond to the noise.
+        Input:
+            - SpectrogramObject: A spectrogram object to be segmented into 
+            foreground and background. 
 
         Output:
             numpy array of pixels indices that are in the foreground (signal)     
     """
-    G = setUpGraph(SpectrogramObject, foregroundPoints, backgroundPoints)
+    print("The graph will now be set up.")
+    G = setUpGraph(SpectrogramObject)
 
     cutVal, partition = minimum_cut(G, "s", "t")
 
@@ -147,7 +112,7 @@ def segmentImage(SpectrogramObject:Spectrogram, foregroundPoints:list, backgroun
 
     print("Everyone Has the edges from s and to t, but floating capacities")
 
-    return np.array(reach_Foreground)
+    return np.array(list(reach_Foreground))
 
     # count = 0
     # for state in reach_Foreground:
