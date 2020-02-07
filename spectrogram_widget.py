7498fbe0f71fd5a2e2220f35ed840d60ab4ef40c
NickHMC <nkoskelo@hmc.edu>
2020-02-07 13:58:17 -0800

    Moved out the percent slider functionality to the controls folder.

--- spectrogram_widget.py
+++ spectrogram_widget.py
@@ -25,53 +25,9 @@ from gaussian_follow import GaussianFitter
 from peak_follower import PeakFollower
 
 from WidgetControls.value_sliders import ValueSlider
+from WidgetControls.percent_slider import PercentSlider # Note that this class is not actually used yet. 02/07/20
 
 DEFMAP = '3w_gby'  # should really be in an .ini file
-
-class PercentSlider(widgets.IntRangeSlider):
-    """
-    A Slider to represent the percentage of a range that we
-    wish to display. The slider maps linearly over the range
-    specified by yrange = (ymin, ymax). Getters and setters
-    are defined for ymin, ymax, and range.
-    """
-
-    def __init__(self, description, initial_value, yrange):
-        super().__init__(
-            description=description,
-            min=0, max=100, readout=True,
-            value=initial_value,
-            layout=widgets.Layout(width='400px')
-        )
-        self._ymin = yrange[0]
-        self._ymax = yrange[1]
-
-    @property
-    def ymin(self):
-        return self._ymin
-
-    @ymin.setter
-    def ymin(self, v):
-        self._ymin = v
-
-    @property
-    def ymax(self):
-        return self._ymax
-
-    @ymax.setter
-    def ymax(self, v):
-        self._ymax = v
-
-    @property
-    def range(self):
-        dy = 0.01 * (self.ymax - self.ymin)
-        return [v * dy + self.ymin for v in self.value]
-
-    @range.setter
-    def range(self, val):
-        assert isinstance(val, (list, tuple)) and len(val) == 2
-        self._ymin, self._ymax = val
-
 
 class SpectrogramWidget:
     """
