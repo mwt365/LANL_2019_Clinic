6f9cb65be9d34492fab0eaa2f156d5e160cabbfb
NickHMC <nkoskelo@hmc.edu>
2020-04-24 18:36:20 -0700

    do not need extra print statements.

--- ImageProcessing/Templates/saveTemplateImages.py
+++ ImageProcessing/Templates/saveTemplateImages.py
@@ -17,7 +17,6 @@ def saveAllTemplateImages():
 	        imsave(f"./im_template_{x}.png", x.value[0])
 
     # Template images saved to the template directory.
-    print("All the template images have been saved.")
 
 def getImageDirectory():
     return os.path.split(os.path.relpath(__file__))[0]
