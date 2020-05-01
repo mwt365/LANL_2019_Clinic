121e7d6b2a64775ea701dead2925e51e70f7d4b7
NickHMC <nkoskelo@hmc.edu>
2020-05-01 14:05:08 -0700

    Tabs vs. four spaces. Use 4 spaces.

--- ImageProcessing/TemplateMatching/Templates/saveTemplateImages.py
+++ ImageProcessing/TemplateMatching/Templates/saveTemplateImages.py
@@ -11,23 +11,24 @@ else:
     from scipy.misc import imsave
 
 def saveAllTemplateImages():
-	currDir = os.getcwd()
-	os.chdir(imageSaveDir)
+    currDir = os.getcwd()
+    imageSaveDir = getImageDirectory()
+    os.chdir(imageSaveDir)
 
     for ind, x in tqdm.tqdm(enumerate(Templates)):
-    	if not os.path.exists(f"./im_template_{x}.png"):
-	        imsave(f"./im_template_{x}.png", x.value[0])
+        if not os.path.exists(f"./im_template_{x}.png"):
+            imsave(f"./im_template_{x}.png", x.value[0])
 
     # Template images saved to the template directory.
 
     os.chdir(currDir)
 
     return imageSaveDir
-    
+
 def getImageDirectory():
     return os.path.split(os.path.relpath(__file__))[0]
 
 
 if __name__ == "__main__":
-	# Save all the image files in the appropriate directory.
+    # Save all the image files in the appropriate directory.
 	saveAllTemplateImages()
